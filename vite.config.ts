e3e4c02c79b6084e7cad149f26c27a41cd39dda7
matheus martins <mathews_2010@outlook.com>
2023-04-09 10:50:45 -0300

    refactor: change the build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
+import dts from 'vite-plugin-dts'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import * as packageJson from './package.json'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    tsConfigPaths(),
+    dts({
+      include: ['src']
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom'
+  },
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'izi-hooks',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `izi-hooks.${format === 'cjs' ? 'cjs' : 'es.js'}`
+    },
+    optimizeDeps: {
+      exclude: Object.keys(packageJson.peerDependencies)
+    },
+    esbuild: {
+      minify: true
+    }
   }
 })
