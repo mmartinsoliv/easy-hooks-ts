ab85275afc8e453058100b057766d3574ecb14e0
matheus martins <mathews_2010@outlook.com>
2023-04-10 11:48:43 -0300

    test: update unit test

--- src/useIsMobile.spec.ts
+++ src/useIsMobile.spec.ts
@@ -3,7 +3,7 @@ import { useCount } from './useIsMobile'
 
 test('should render the useCount', () => {
   const { result } = renderHook(useCount)
-  expect(result.current.count).toBe(1)
+  expect(result.current.count).toBe(0)
   act(() => result.current.handleCount())
-  expect(result.current.count).toBe(2)
+  expect(result.current.count).toBe(1)
 })
