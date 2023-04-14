a3da23943b7f17b16674f78e653b6fed1a3309d0
matheus martins <mathews_2010@outlook.com>
2023-04-14 10:00:36 -0300

    refactor(usemedia): remove custom hook

--- src/useMedia/useMedia.spec.ts
+++
@@ -1,9 +0,0 @@
-import { renderHook, act } from '@testing-library/react'
-import { useMedia } from './useMedia'
-
-test('should render the useMedia', () => {
-  const { result } = renderHook(useMedia)
-  expect(result.current.count).toBe(0)
-  act(() => result.current.handleCount())
-  expect(result.current.count).toBe(1)
-})
