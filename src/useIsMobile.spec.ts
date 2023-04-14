4c702ab8db7e0e92442ba50739145a39d56d7364
matheus martins <mathews_2010@outlook.com>
2023-04-14 10:07:22 -0300

    refactor: rename the function

--- src/useIsMobile.spec.ts
+++ src/useIsMobile.spec.ts
@@ -4,6 +4,6 @@ import { useCount } from './useIsMobile'
 test('should render the useCount', () => {
   const { result } = renderHook(useCount)
   expect(result.current.count).toBe(0)
-  act(() => result.current.handleCount())
+  act(() => result.current.handleClick())
   expect(result.current.count).toBe(1)
 })
--- src/useIsMobile.ts
+++ src/useIsMobile.ts
@@ -3,9 +3,9 @@ import { useState } from 'react'
 export const useCount = () => {
   const [count, setCount] = useState(0)
 
-  const handleCount = () => {
+  const handleClick = () => {
     setCount(count + 1)
   }
 
-  return { count, handleCount }
+  return { count, handleClick }
 }
