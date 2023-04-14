70b6fb800f99abb9c0af4684f4dadda1c29d75d6
matheus martins <mathews_2010@outlook.com>
2023-04-14 00:08:25 -0300

    build(tsup): change the name to define config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  name: 'izi-hooks',
+  name: 'easy-hooks-ts',
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   outDir: 'dist',
