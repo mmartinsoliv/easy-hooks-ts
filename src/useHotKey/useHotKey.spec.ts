57b2f3fcbe894dadd9d8b078dece3394a4fbe9da
matheus martins <mathews_2010@outlook.com>
2023-04-13 23:35:40 -0300

    fix: update

--- src/useHotKey/useHotKey.spec.ts
+++ src/useHotKey/useHotKey.spec.ts
@@ -3,7 +3,7 @@ import { useHotKey } from './useHotKey'
 
 test('should render the useHotKey', () => {
   const { result } = renderHook(useHotKey)
-  expect(result.current.count).toBe(0)
-  act(() => result.current.handleCount())
   expect(result.current.count).toBe(1)
+  act(() => result.current.handleCount())
+  expect(result.current.count).toBe(2)
 })
--- src/useHotKey/useHotKey.ts
+++ src/useHotKey/useHotKey.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export const useHotKey = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   const handleCount = () => {
     setCount(count + 1)
