d0d9bad9489f11173d04d91d3219ffc73a2699ee
matheus martins <mathews_2010@outlook.com>
2023-04-14 14:31:37 -0300

    chore: remove useIsMobile hook

--- src/useIsMobile.spec.ts
+++
@@ -1,9 +0,0 @@
-import { renderHook, act } from '@testing-library/react'
-import { useCount } from './useIsMobile'
-
-test('should render the useCount', () => {
-  const { result } = renderHook(useCount)
-  expect(result.current.count).toBe(0)
-  act(() => result.current.handleClick())
-  expect(result.current.count).toBe(1)
-})
