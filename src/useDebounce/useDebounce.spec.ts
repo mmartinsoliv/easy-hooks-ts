e9ad7c52d0208fd4e4dc4b350695b162a59284c9
matheus martins <mathews_2010@outlook.com>
2023-04-18 11:23:26 -0300

    refactor(debounce): change the initial value

--- src/useDebounce/useDebounce.spec.ts
+++ src/useDebounce/useDebounce.spec.ts
@@ -4,26 +4,26 @@ import { useDebounce } from './useDebounce'
 
 describe('useDebounce', () => {
   test('should return the initial value', () => {
-    const { result } = renderHook(() => useDebounce('hello', 500))
+    const { result } = renderHook(() => useDebounce('hello', 1000))
 
     expect(result.current).toBe('hello')
   })
 
   test('should return the debounced value', async () => {
     const { result, rerender } = renderHook(({ value, delay }) => useDebounce(value, delay), {
-      initialProps: { value: 'hello', delay: 500 }
+      initialProps: { value: 'hello', delay: 1000 }
     })
 
     expect(result.current).toBe('hello')
 
     // Rerender with a new value
-    rerender({ value: 'world', delay: 500 })
+    rerender({ value: 'world', delay: 1000 })
 
     // Should still be the old value
     expect(result.current).toBe('hello')
 
     // Fast-forward time by 500ms
-    await new Promise((resolve) => setTimeout(resolve, 500))
+    await new Promise((resolve) => setTimeout(resolve, 1000))
 
     // Should now be the new value
     expect(result.current).toBe('hello')
--- src/useDebounce/useDebounce.ts
+++ src/useDebounce/useDebounce.ts
@@ -4,7 +4,7 @@ export const useDebounce = <T>(value: T, delay?: number): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value)
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500)
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 1000)
 
     return () => {
       clearTimeout(timer)
