cd9493f3da1cafafdb394bf341d7de270d0bd40a
matheus martins <mathews_2010@outlook.com>
2023-04-24 11:27:18 -0300

    chore: remove the exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 export { useToggle } from './useToggle/useToggle'
 
-export { useClipboard } from './useClipboard/useClipboard'
-
-export { useDebounce } from './useDebounce/useDebounce'
-
 export { useFetch } from './useFetch/useFetch'
 
 export { useCounter } from './useCounter/useCounter'
